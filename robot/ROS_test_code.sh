dc1862ce72003e73e4b8e4bcee5dda4a1d8f588b
박철희 <chdmb@naver.com>
2020-05-23 15:41:18 +0900

    2주차 강의내용 추가 및 코드 내용 추가

--- robot/ROS_test_code.sh
+++ robot/ROS_test_code.sh
@@ -7,3 +7,18 @@ rosrun turtlesim turtlesim_node
 rosrun turtlesim turtle_teleop_key
 
 rosrun rqt_graph rqt_graph
+
+
+# 카메라 사용을 위해, 다음의 패키지 설치
+sudo apt-get install ros-kinetic-uvc-camera
+
+# 이 코드는 카메라를 이용한 실습. 카메라는 /dev/video0 에 등록되어 있음 
+rosrun uvc_camera uvc_camera_node _device:=/dev/video0
+
+# rqt의 경우, 에러가 발생해 rqt 실행 후 플러그인 선택하는 식으로 진행
+rqt
+
+# 센서에서 나오는 데이터 확인.
+rostopic echo /image_raw
+rostopic echo /turtle1/pose
+
