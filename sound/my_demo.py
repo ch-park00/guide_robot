75769a91da1f4463bad18e6f1cae1ac3bc737b6c
pch <chdmb@naver.com>
2020-11-05 11:37:54 +0900

    my_demo.py 코드 수정

--- sound/my_demo.py
+++ sound/my_demo.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*- 
+
+
 import snowboydecoder
 import sys
 import signal
 import os
-import googlesamples.assistant.grpc.pushtotalk_fix as g_assist 
+import googlesamples.assistant.grpc.my_assist as g_assist 
 import wave
-
+import subprocess
+import time
 """
 This demo file shows you how to use the new_message_callback to interact with
 the recorded audio after a keyword is spoken. It uses the speech recognition
@@ -17,17 +21,44 @@ https://pypi.python.org/pypi/SpeechRecognition/
 
 interrupted = False
 
-
 def detectedCallback():
-  # 소리 출력
-  #play_audio()
-  snowboydecoder.play_audio_file()
+  global my
   # 핵심어 검출 종료
   detector.terminate()
+  # 소리 출력
+  snowboydecoder.play_audio_file()
+  # 로봇 정지 플래그 보내기
+  start=time.time()
+  subprocess.Popen(["rosrun","input_msg", "input_talker", "1"])
+  print(time.time()-start)
   # 어시스턴스 실행
   a = g_assist.main()
+  flag=0
   print(a)
-  # 핵심어 검출 재수행
+  # 목적지 검출
+  if len(a)>0:
+      if "고기" in a[0]:
+          flag+=2
+      if "야채" in a[0]:
+          flag+=4
+      if "과일" in a[0]:
+          flag+=8
+      if "과자" in a[0]:
+          flag+=16
+      if "음료" in a[0]:
+          flag+=32
+      if "견과류" in a[0]:
+          flag+=64
+      if "라면" in a[0]:
+          flag+=128
+      if "위생" in a[0]:
+          flag+=256
+  print(a,flag)
+#  f=open('test.txt','w+')
+#  f.write(str(flag))
+#  f.close()
+  # 목적지 정보 플래그로 보내기
+  subprocess.Popen(["rosrun","input_msg", "input_talker", str(flag)])
   detector.start(detected_callback=detectedCallback,
                interrupt_check=interrupt_callback,
                sleep_time=0.01)
@@ -42,17 +73,13 @@ def interrupt_callback():
     global interrupted
     return interrupted
 
-#if len(sys.argv) == 1:
-#    print("Error: need to specify model name")
-#    print("Usage: python demo.py your.model")
-#    sys.exit(-1)
 
 model = 'resources/models/snowboy.umdl'
 
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.38)
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.7)
 print('Listening... Press Ctrl+C to exit')
 
 # main loop
