a89b532496eacf9eb12ef61d0ee32ed63b078395
최연균 <ykc0114@naver.com>
2020-11-27 14:48:55 +0900

    Input order message for standalone environment

---
+++ Navigation Script/order_msg.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import rospy
+from std_msgs.msg import Int32
+    
+if __name__ == "__main__":
+    try:
+        pub = rospy.Publisher('inputOrder', Int32, queue_size=1)
+        rospy.init_node('Order', anonymous=True)
+        while not rospy.is_shutdown():
+            # 0      : default
+            # -1     : stop
+            # -2     : LOW_BATTERY
+            # others : publish destination
+            flag = input()
+
+            pub.publish(flag)
+            rospy.loginfo("Order published : %d", flag)
+            
+
+    except rospy.ROSInterruptException:
+        pass
